8a122d21fb11b927c70903a0e6c30aaae79f731b
Batman <marcin.galeziowski@gmail.com>
2018-01-24 15:22:47 +0100

    locationsSaga watcher test

--- __tests__/locationsSaga.spec.js
+++ __tests__/locationsSaga.spec.js
@@ -1,8 +1,8 @@
-import { takeEvery, delay } from 'redux-saga'
+import { takeEvery, delay, takeLatest, fork } from 'redux-saga'
 import { expectSaga } from 'redux-saga-test-plan';
 import { call, put, take } from 'redux-saga/effects'
 import * as LocationsActions from '../src/redux/actions/locationsActions';
-import { citySearchSaga } from '../src/redux/sagas/locationsSaga';
+import locationsSagas, { citySearchSaga } from '../src/redux/sagas/locationsSaga';
 import { CITY_SEARCH, CITY_SEARCH_FAILED } from '../src/redux/actions/actionTypes';
 import * as matchers from 'redux-saga-test-plan/matchers';
 import { citySearchRequest } from '../src/api/locationsAPI';
@@ -12,8 +12,16 @@ import { citySearch } from '../src/routes/routesDefinitions';
 
 //*******************************************************************************************************//
 
-describe('>>>> test locationsSaga', () => {
-    it('should have the citySearchSaga', () => expectSaga(citySearchSaga, { payload: { query: '' } })
+describe('>>>> test locationsSagas', () => {
+    it('should have the expected watchers', done => {
+        expectSaga(locationsSagas)
+            .run({ silenceTimeout: true })
+            .then(saga => {
+                expect(saga).toMatchSnapshot();
+                done();
+            });
+    });
+    it('should put a CITY_SEARCH_SUCCESS', () => expectSaga(citySearchSaga, { payload: { query: '' } })
         .provide([
             // Use the `call.fn` matcher from Redux Saga Test Plan
             [matchers.call.fn(citySearchRequest), [{ city: 'Oslo' }]],
@@ -24,21 +32,21 @@ describe('>>>> test locationsSaga', () => {
         })
         .run()
     );
+    it('should throw an error on locationsSaga', () => {
+        const generator = citySearchSaga(LocationsActions.citySearch({ query: 'Oslo' }));
+        const error = new Error('Failed to fetch cities');
+        assert.deepEqual(
+            generator.next().value,
+            call(citySearchRequest, 'Oslo'),
+            'waiting for fetch response'
+        )
+        assert.deepEqual(
+            generator.throw(error).value,
+            put({ type: 'CITY_SEARCH_FAILED', payload: { error } }),
+            'putting failure action'
+        )
+    });
 });
 
-it('throws error on locationsSaga', () => {
-    const generator = citySearchSaga(LocationsActions.citySearch({ query: 'Oslo' }));
-    const error = new Error('Failed to fetch cities');
-    assert.deepEqual(
-        generator.next().value,
-        call(citySearchRequest, 'Oslo'),
-        'waiting for fetch response'
-    )
-    assert.deepEqual(
-      generator.throw(error).value,
-      put({ type: 'CITY_SEARCH_FAILED', payload: {error} }),
-      'putting failure action'
-    )
-});
 
 // assert.deepEqual(iterator.throw('error').value, put(actions.checkoutFailure('error')))
